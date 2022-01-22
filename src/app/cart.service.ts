8cac7918a777465b65f3cbd6e0702a94e289d465
marti <martin.angermann@gmail.com>
2022-01-22 23:03:41 +0100

    added payment and order

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 import { Product } from './model/product';
 import { CartItem } from './model/cartItem';
 import { Observable, of } from 'rxjs';
-
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { AuthorizationService } from './authorization/authorization.service';
+import { OrderData } from './model/orderData';
+import { OrderItemsData } from './model/orderItemsData';
+const baseUrl = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +15,16 @@ import { Observable, of } from 'rxjs';
 export class CartService {
   private cartItems: CartItem[];
 
-  constructor() {
+  constructor(private http: HttpClient, private authorization: AuthorizationService) {
     this.cartItems = [];
   }
 
   public getItems(): Observable<CartItem[]> {
     return of(this.cartItems);
+  }
+
+  public getItemsForOrder(): CartItem[] {
+    return this.cartItems;
   }
 
   public addItem(product: Product) {
@@ -71,5 +80,58 @@ export class CartService {
 
     return ids;
   }
+
+  public pay(cardNr: number, cardSecret: number) :Observable<string>{
+    let httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    let options = {
+        headers: httpHeaders
+    }
+
+
+    return this.http.post<string>(baseUrl + '/payment', '{"cardNr": "' + cardNr + '", "cardSecret": "' + cardSecret + '"}', options);   
+    
+  }
+
+  public order(orderData: OrderData) :Observable<string>{
+    let httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    let options = {
+        headers: httpHeaders
+    }
+
+
+    return this.http.post<string>(baseUrl + '/order', orderData, options);   
+   
+    
+  }
+
+  public getOidByToken(token: string) :Observable<string> {
+    let httpHeaders = new HttpHeaders({
+        'Authorization': this.authorization.getToken(),
+    });
+    console.log("in get oid" + token);
+    return this.http.get<string>(baseUrl + '/getOrderIdbyToken/' + token);
+  }
+
+  public insertInOrederedProducts(id: number) :Observable<string>{
+    let httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    let options = {
+        headers: httpHeaders
+    }
+
+    let data = new OrderItemsData(id, this.cartItems);
+    
+
+
+
+    return this.http.post<string>(baseUrl + '/testPost', data, options); 
+  }
+
+
 
 }
